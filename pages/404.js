af8b095fe724108755c7871774efc7732bcbbaf9
keil <keilwmartin@gmail.com>
2022-03-25 18:48:53 -0400

    build error fixed?

--- pages/404.js
+++ pages/404.js
@@ -7,12 +7,12 @@ const NotFound = () => {
     <Layout>
       <div className="flex justify-center items-center h-[80vh]">
         <div>
-          <h1 className="text-center md:text-left mb-6 md:mb-10 font-serif text-4xl md:text-8xl">
+          <h1 className="mb-6 font-serif text-4xl text-center md:text-left md:mb-10 md:text-8xl">
             404: Oops!
           </h1>
           <div className="md:ml-3">
             <p className="mb-2 text-lg text-secondary">
-              I can't seem to find the page you're looking for.
+              I can&apos;t seem to find the page you&apos;re looking for.
             </p>
             <Link href="/">Go Home</Link>
           </div>
