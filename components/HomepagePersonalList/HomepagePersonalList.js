b3ef5a05ea803f7d170ddea47c5061a36d1f00d2
keil <keilwmartin@gmail.com>
2022-04-07 21:40:41 -0400

    updating project listings

--- components/HomepagePersonalList/HomepagePersonalList.js
+++ components/HomepagePersonalList/HomepagePersonalList.js
@@ -4,76 +4,52 @@ import Link from "next/link";
 const PersonalProjectList = ({ personalProjects }) => {
   return (
     <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 md:gap-10">
-      <li key="MOCKUP7" className="pb-6 border-b-2">
-        <Link href="/">
-          <a className="relative inline-block w-full h-48 pt-8 pl-8 mb-4 overflow-hidden transition duration-300 ease-in-out bg-gray-100 rounded-lg hover:shadow-lg hover:scale-105">
-            <div className="absolute w-full overflow-hidden rounded-tl-3xl top-10">
-              <Image
-                src="/pictures/project.png"
-                alt="MOCKUP8"
-                width={768}
-                height={384}
-                objectFit="cover"
-                objectPosition="top left"
-              />
-            </div>
-          </a>
-        </Link>
-        <h3 className={`font-semibold text-xl text-primary mb-1`}>
-          Project Title
-        </h3>
-        <span className={"block text-md text-tertiary mb-1"}>
-          Project Subtitle
-        </span>
-        <Link href={"/mockProject"}>
-          <a className="mt-2 btn-light">View Project</a>
-        </Link>
-      </li>
-
-      <li key="MOCKUP" className="pb-6 border-b-2">
-        <Link href="/">
-          <a className="relative inline-block w-full h-48 pt-8 pl-8 mb-4 overflow-hidden transition duration-300 ease-in-out bg-gray-100 rounded-lg hover:shadow-lg hover:scale-105">
-            <div className="absolute w-full overflow-hidden rounded-tl-3xl top-10">
-              <Image
-                src="/pictures/project.png"
-                alt="MOCKUP9"
-                width={768}
-                height={384}
-                objectFit="cover"
-                objectPosition="top left"
-              />
-            </div>
-          </a>
-        </Link>
-        <h3 className={`font-semibold text-xl text-primary mb-1`}>
-          Project Title
-        </h3>
-        <span className={"block text-md text-tertiary mb-1"}>
-          Project Subtitle
-        </span>
-        <Link href={"/mockProject"}>
-          <a className="mt-2 btn-light">View Project</a>
-        </Link>
-      </li>
       {personalProjects.map((personalProject) => (
-        <div key={personalProject.id}>
-          <img
-            src={personalProject.image}
-            alt={personalProject.name}
-            width={420}
-            height={300}
-            objectFit="cover"
-            objectPosition="top left"
-          />
-          <div className="flex flex-col">
-            <div>{personalProject.name}</div>
-            <div>{personalProject.timestamp}</div>
-            <div>{personalProject.description}</div>
-            <div>{personalProject.stack}</div>
-            <a href={personalProject.url}>Link here</a>
-            <div>{personalProject.company}</div>
-          </div>
-        </div>
+        <li key={personalProject.id} className="pb-6 border-b-2">
+          <Link href="/">
+            <a className="relative inline-block w-full h-48 pt-8 pl-8 mb-4 overflow-hidden transition duration-300 ease-in-out bg-gray-100 rounded-lg hover:shadow-lg hover:scale-105">
+              <div className="absolute w-full overflow-hidden rounded-tl-3xl top-10">
+                <img
+                  src={personalProject.image}
+                  alt={personalProject.name}
+                  width={768}
+                  height={384}
+                  objectFit="cover"
+                  objectPosition="top left"
+                />
+              </div>
+            </a>
+          </Link>
+          <h3 className={`font-semibold text-xl text-primary mb-1`}>
+            {personalProject.name}
+          </h3>
+          <span className={"block text-md text-tertiary mb-1"}>
+            {personalProject.stack}
+          </span>
+          <Link href={personalProject.url}>
+            <a className="mt-2 btn-light">View Project</a>
+          </Link>
+        </li>
+        //   {personalProjects.map((personalProject) => (
+        //     <div key={personalProject.id}>
+        //       <img
+        //         src={personalProject.image}
+        //         alt={personalProject.name}
+        //         width={420}
+        //         height={300}
+        //         objectFit="cover"
+        //         objectPosition="top left"
+        //       />
+        //       <div className="flex flex-col">
+        //         <div>{personalProject.name}</div>
+        //         <div>{personalProject.timestamp}</div>
+        //         <div>{personalProject.description}</div>
+        //         <div>{personalProject.stack}</div>
+        //         <a href={personalProject.url}>Link here</a>
+        //         <div>{personalProject.company}</div>
+        //       </div>
+        //     </div>
+        // ))}
       ))}
     </ul>
   );
--- components/HomepageWorkList/HomepageWorkList.js
+++ components/HomepageWorkList/HomepageWorkList.js
@@ -4,37 +4,42 @@ import Link from "next/link";
 const HomepageWorkList = ({ workProjects }) => {
   return (
     <ul className="pb-12 space-y-12">
-      <li className="flex flex-col items-center space-x-0 space-y-4 md:flex-row md:space-y-0 md:space-x-8 lg:space-x-12">
-        <Link href="/">
-          <a className="flex w-full px-8 pt-8 overflow-hidden transition duration-500 ease-in-out bg-gray-100 rounded-lg md:px-12 md:pt-12 flex-end md:w-1/2 lg:w-2/3 h-72 hover:shadow-lg hover:scale-105">
-            <div className="flex overflow-hidden shadow-md rounded-t-md">
-              <Image
-                src="/pictures/project.png"
-                alt="MOCKUP10"
-                width={768}
-                height={384}
-                objectFit="cover"
-                objectPosition="top left"
-              />
-            </div>
-          </a>
-        </Link>
-        <div className="w-full md:w-1/2 lg:w-1/3">
-          <span className="block mb-1 text-xs font-semibold tracking-widest uppercase text-tertiary">
-            Company Name
-          </span>
-          <h3 className="mb-1 text-xl font-semibold text-primary">
-            Project Title
-          </h3>
-          <span className="block mb-1 text-md text-tertiary">
-            Project Subtitle
-          </span>
-          <Link href="/">
-            <a className="w-full mt-2 btn-light md:w-auto">View Project</a>
-          </Link>
-        </div>
-      </li>
       {workProjects.map((workProject) => (
+        <li
+          className="flex flex-col items-center space-x-0 space-y-4 md:flex-row md:space-y-0 md:space-x-8 lg:space-x-12"
+          key={workProject.id}
+        >
+          <Link href="/">
+            <a className="flex w-full px-8 pt-8 overflow-hidden transition duration-500 ease-in-out bg-gray-100 rounded-lg md:px-12 md:pt-12 flex-end md:w-1/2 lg:w-2/3 h-72 hover:shadow-lg hover:scale-105">
+              <div className="flex overflow-hidden shadow-md rounded-t-md">
+                <img
+                  src={workProject.image}
+                  alt={workProject.company}
+                  width={768}
+                  height={384}
+                  objectFit="cover"
+                  objectPosition="top left"
+                />
+              </div>
+            </a>
+          </Link>
+          <div className="w-full md:w-1/2 lg:w-1/3">
+            <span className="block mb-1 text-xs font-semibold tracking-widest uppercase text-tertiary">
+              {workProject.timestamp}
+            </span>
+            <h3 className="mb-1 text-xl font-semibold text-primary">
+              {workProject.company}
+            </h3>
+            <span className="block mb-1 text-md text-tertiary">
+              {workProject.stack}
+            </span>
+            <Link href={workProject.url}>
+              <a className="w-full mt-2 btn-light md:w-auto">View Project</a>
+            </Link>
+          </div>
+        </li>
+      ))}
+      {/* {workProjects.map((workProject) => (
         <div key={workProject.id}>
           <img
             src={workProject.image}
@@ -53,7 +58,7 @@ const HomepageWorkList = ({ workProjects }) => {
             <div>{workProject.company}</div>
           </div>
         </div>
-      ))}
+      ))} */}
     </ul>
   );
 };
