370b326c562a1e0a5468767721a79ef6793d4231
keil <keilwmartin@gmail.com>
2022-07-21 12:49:08 -0400

    screen read for toggle + svg update

--- components/Nav/ThemeToggle.js
+++ components/Nav/ThemeToggle.js
@@ -12,16 +12,23 @@ const ThemeToggle = () => {
   return (
     <button
       type="button"
-      role="checkbox"
+      role="switch"
       onClick={toggleTheme}
+      aria-checked={theme === "dark" ? true : false}
       className="relative inline-flex w-12 transition-colors duration-500 ease-in-out bg-gray-200 border-2 border-transparent rounded-full cursor-pointer dark:bg-darkModeSecondary shrink-0 h-7 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
     >
+      <span className="sr-only">
+        Toggle {theme === "dark" ? "light" : "dark"} mode
+      </span>
       <span
         className={`${
           theme === "dark" ? "toggle-active" : ""
         } relative inline-block w-6 h-6 transition duration-200 ease-in-out bg-white rounded-full shadow pointer-events-none dark:bg-syncWave ring-0`}
       >
-        <span className="absolute inset-0 flex items-center justify-center w-full h-full transition-opacity -rotate-[40deg]">
+        <span
+          aria-hidden={true}
+          className="absolute inset-0 flex items-center justify-center w-full h-full transition-opacity -rotate-[40deg]"
+        >
           {theme === "dark" ? (
             <DarkMoon xmlns="http://www.w3.org/2000/svg" />
           ) : (
