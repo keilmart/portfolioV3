aeca66debd6f9bd69bfcebe6026194650692375f
keil <keilwmartin@gmail.com>
2022-10-17 10:20:48 -0400

    update and clean up

--- components/Homepage/HomepageIcons/Index.js
+++ components/Homepage/HomepageIcons/Index.js
@@ -1,3 +1,78 @@
+// This is the blob graphic on the Hero.js //
+export function Blob() {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      id="sw-js-blob-svg"
+      viewBox="0 0 100 100"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <linearGradient id="sw-gradient" x1="0" x2="1" y1="1" y2="0">
+          <stop
+            id="stop1"
+            stopColor="rgba(106, 152, 240, 1)"
+            offset="0%"
+          ></stop>
+          <stop
+            id="stop2"
+            stopColor="rgba(73, 97, 220, 1)"
+            offset="100%"
+          ></stop>
+        </linearGradient>
+      </defs>
+      <path
+        fill="url(#sw-gradient)"
+        d="M19.7,-28.9C26.5,-26.3,33.6,-22.4,38.3,-16.3C43,-10.1,45.2,-1.7,43.1,5.5C41.1,12.7,34.7,18.7,29.1,25.3C23.5,31.9,18.6,39.3,12,41.4C5.5,43.6,-2.8,40.5,-9.9,37C-17.1,33.4,-23.2,29.4,-27.8,24.1C-32.4,18.8,-35.5,12.1,-37.4,4.8C-39.3,-2.5,-40.1,-10.6,-36.7,-16.2C-33.3,-21.8,-25.7,-25,-18.9,-27.7C-12.1,-30.3,-6,-32.5,0.2,-32.8C6.4,-33.2,12.9,-31.6,19.7,-28.9Z"
+        width="100%"
+        height="100%"
+        transform="translate(50 50)"
+        strokeWidth="0"
+      ></path>
+    </svg>
+  );
+}
+
+// This is located on the about page //
+export function TriSquareIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="62"
+      height="42"
+      fill="none"
+      viewBox="0 0 62 42"
+    >
+      <g strokeWidth="2" clipPath="url(#clip0_611_4813)">
+        <path
+          fill="#999999"
+          stroke="#999999"
+          d="M1.304 12.631H31.304V42.631H1.304z"
+          transform="rotate(-22.184 1.304 12.631)"
+        ></path>
+        <path
+          fill="#E5E7EB"
+          stroke="#E5E7EB"
+          d="M22.414 24.109H44.414V46.109H22.414z"
+          transform="rotate(-40.536 22.414 24.11)"
+        ></path>
+        <path
+          fill="#6d83f2"
+          stroke="#6d83f2"
+          d="M46.395 25.485H56.395V35.485H46.395z"
+          transform="rotate(-45 46.395 25.485)"
+        ></path>
+      </g>
+      <defs>
+        <clipPath id="clip0_611_4813">
+          <path fill="#fff" d="M0 0H62V42H0z"></path>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
 export function LinkedIcon() {
   return (
     <svg className="svg-icon" viewBox="0 0 24 24" width="1em" height="1em">
@@ -173,81 +248,6 @@ export function SquirqleIcon() {
           transform="rotate(-90 -26.258 14.297)"
         ></path>
       </g>
-    </svg>
-  );
-}
-
-// This is the blob graphic on the Hero.js //
-export function Blob() {
-  return (
-    <svg
-      width="100%"
-      height="100%"
-      id="sw-js-blob-svg"
-      viewBox="0 0 100 100"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <defs>
-        <linearGradient id="sw-gradient" x1="0" x2="1" y1="1" y2="0">
-          <stop
-            id="stop1"
-            stopColor="rgba(106, 152, 240, 1)"
-            offset="0%"
-          ></stop>
-          <stop
-            id="stop2"
-            stopColor="rgba(73, 97, 220, 1)"
-            offset="100%"
-          ></stop>
-        </linearGradient>
-      </defs>
-      <path
-        fill="url(#sw-gradient)"
-        d="M19.7,-28.9C26.5,-26.3,33.6,-22.4,38.3,-16.3C43,-10.1,45.2,-1.7,43.1,5.5C41.1,12.7,34.7,18.7,29.1,25.3C23.5,31.9,18.6,39.3,12,41.4C5.5,43.6,-2.8,40.5,-9.9,37C-17.1,33.4,-23.2,29.4,-27.8,24.1C-32.4,18.8,-35.5,12.1,-37.4,4.8C-39.3,-2.5,-40.1,-10.6,-36.7,-16.2C-33.3,-21.8,-25.7,-25,-18.9,-27.7C-12.1,-30.3,-6,-32.5,0.2,-32.8C6.4,-33.2,12.9,-31.6,19.7,-28.9Z"
-        width="100%"
-        height="100%"
-        transform="translate(50 50)"
-        strokeWidth="0"
-      ></path>
-    </svg>
-  );
-}
-
-// This is located on the about page //
-export function TriSquareIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="62"
-      height="42"
-      fill="none"
-      viewBox="0 0 62 42"
-    >
-      <g strokeWidth="2" clipPath="url(#clip0_611_4813)">
-        <path
-          fill="#999999"
-          stroke="#999999"
-          d="M1.304 12.631H31.304V42.631H1.304z"
-          transform="rotate(-22.184 1.304 12.631)"
-        ></path>
-        <path
-          fill="#E5E7EB"
-          stroke="#E5E7EB"
-          d="M22.414 24.109H44.414V46.109H22.414z"
-          transform="rotate(-40.536 22.414 24.11)"
-        ></path>
-        <path
-          fill="#6d83f2"
-          stroke="#6d83f2"
-          d="M46.395 25.485H56.395V35.485H46.395z"
-          transform="rotate(-45 46.395 25.485)"
-        ></path>
-      </g>
-      <defs>
-        <clipPath id="clip0_611_4813">
-          <path fill="#fff" d="M0 0H62V42H0z"></path>
-        </clipPath>
-      </defs>
     </svg>
   );
 }
