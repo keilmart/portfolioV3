88f8078844bcdef099b1bfdbad132ccd1c10e2e6
keil <keilwmartin@gmail.com>
2022-11-17 14:58:36 -0500

    Remove pointer events on code block - mobile

--- components/Homepage/CodeBlock.js
+++ components/Homepage/CodeBlock.js
@@ -1,6 +1,6 @@
 const HomepageSkillsList = () => {
   return (
-    <div className="relative z-10 px-6 py-4 antialiased transition duration-500 ease-in-out bg-gray-100 border-2 rounded-lg text-2xs text-syncWave lg:px-10 lg:py-8 lg:text-sm dark:text-slate-200 dark:bg-darkModeDetail dark:border-transparent">
+    <div className="relative z-10 px-6 py-4 antialiased transition duration-500 ease-in-out bg-gray-100 border-2 rounded-lg pointer-events-none text-2xs text-syncWave lg:px-10 lg:py-8 lg:text-sm dark:text-slate-200 dark:bg-darkModeDetail dark:border-transparent">
       <pre className="m-2 sm:m-3 font-montserrat">
         1&nbsp;&nbsp;class <b>Person</b> {"{"}
       </pre>
@@ -11,8 +11,7 @@ const HomepageSkillsList = () => {
       <a
         href="https://www.google.com/search?q=da+nang+pictures"
         target="_blank"
-        rel="noreferrer"
-      >
+        rel="noreferrer">
         <pre className="m-2 sm:m-3 font-montserrat">
           4&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; this.passion = [&quot;
           <b className="uppercase">traveling</b>&quot;];
@@ -27,8 +26,7 @@ const HomepageSkillsList = () => {
       <a
         href="https://www.instagram.com/charlieistheshiht/"
         target="_blank"
-        rel="noreferrer"
-      >
+        rel="noreferrer">
         <pre className="m-2 sm:m-3 font-montserrat">
           3&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; this.myDog = {"{"}
           &quot;name:&nbsp;&nbsp;<b>Charlie</b>&quot;{"}"};
