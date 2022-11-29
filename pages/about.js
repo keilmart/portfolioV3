d5ef55def20d96867f42707a5c3740a4a8e0f175
keil <keilwmartin@gmail.com>
2022-11-29 09:41:03 -0500

    Resume update

--- pages/about.js
+++ pages/about.js
@@ -14,8 +14,7 @@ const RotateIn = ({ children }) => {
       className="hidden overflow-hidden md:block md:w-40 md:h-40"
       initial={{ rotate: 10 }}
       animate={{ rotate: 0 }}
-      transition={{ type: "spring", bounce: 0.25, mass: 2 }}
-    >
+      transition={{ type: "spring", bounce: 0.25, mass: 2 }}>
       {children}
     </motion.div>
   );
@@ -27,8 +26,7 @@ const ExternalLink = ({ href, title }) => {
       href={href}
       target="_blank"
       rel="noreferrer"
-      className="flex items-center mt-1 text-sm border-b border-gray-700 text-secondary hover:text-syncWave hover:border-syncWave hover:bg-gray-100 dark:hover:bg-transparent dark:text-slate-200 dark:border-slate-200 dark:hover:text-syncWave dark:hover:border-syncWave"
-    >
+      className="flex items-center mt-1 text-sm border-b border-gray-700 text-secondary hover:text-syncWave hover:border-syncWave hover:bg-gray-100 dark:hover:bg-transparent dark:text-slate-200 dark:border-slate-200 dark:hover:text-syncWave dark:hover:border-syncWave">
       <span className="mr-1">{title}</span>
       <span className="hidden mb-1 leading-snug sm:block">↗</span>
     </a>
@@ -49,8 +47,7 @@ const About = () => {
             <a
               href="https://www.instagram.com/charlieistheshiht/"
               target="_blank"
-              rel="noreferrer"
-            >
+              rel="noreferrer">
               <Image
                 src="/pictures/me-charlie.png"
                 alt="Personal photo of Keil Martin and Charlie"
@@ -70,16 +67,14 @@ const About = () => {
         </h1>
         <div>
           <p className="mb-4">
-            Building creative tech solutions is my biggest passion, and coding
-            has been a life-long hobby for me. I started like most others
-            playing around in Myspace and the love of it has continued to grow
-            over the years.
+            Building creative tech solutions is my biggest passion, and coding has been a
+            life-long hobby for me. I started like most others playing around in Myspace
+            and the love of it has continued to grow over the years.
           </p>
           <p className="mb-4">
-            As a child I was always taking apart my Playstations and laptops
-            trying to figure out how everything worked and that need to tinker
-            has never dissipated (although my game systems were never the same
-            &nbsp;
+            As a child I was always taking apart my Playstations and laptops trying to
+            figure out how everything worked and that need to tinker has never dissipated
+            (although my game systems were never the same &nbsp;
             <Emoji symbol="🤯" label="Mind blown" />
             &nbsp;
             <Emoji symbol="💥" label="Explosion" />
@@ -92,10 +87,7 @@ const About = () => {
         <ul className="flex items-baseline space-x-4">
           <p className="text-sm dark:text-slate-200">Find me on</p>
           <li>
-            <ExternalLink
-              href="https://www.linkedin.com/in/keilmart/"
-              title="LinkedIn"
-            />
+            <ExternalLink href="https://www.linkedin.com/in/keilmart/" title="LinkedIn" />
           </li>
           <li>
             <ExternalLink href="https://github.com/keilmart" title="Github" />
@@ -111,21 +103,21 @@ const About = () => {
         </h2>
         <div>
           <p>
-            My journey started in the Electrical Engineering Technology program
-            at Georgian College, I knew growing up I always wanted to work with
-            technology, although I wasn&apos;t sure what aspect I wanted to
-            pursue (hardware, software, robotics, etc.). It was in my third year
-            that I started programming in my PLC class. I started by programming
-            and connecting a Raspberry Pi to a live circuit. When I saw my code
-            being translated to physical movements, I knew this was the right
-            fit for me. From that day onward I knew I wanted to be a programmer.
+            My journey started in the Electrical Engineering Technology program at
+            Georgian College, I knew growing up I always wanted to work with technology,
+            although I wasn&apos;t sure what aspect I wanted to pursue (hardware,
+            software, robotics, etc.). It was in my third year that I started programming
+            in my PLC class. I started by programming and connecting a Raspberry Pi to a
+            live circuit. When I saw my code being translated to physical movements, I
+            knew this was the right fit for me. From that day onward I knew I wanted to be
+            a programmer.
           </p>
           <br />
           <p className="mb-4">
-            In 2019, I decided to pursue a formal education in web development
-            at Juno College of Technology (formerly HackerYou), where I
-            completed the Web Development Immersive bootcamp. This gave me all
-            the tools necessary to become a Front End developer.
+            In 2019, I decided to pursue a formal education in web development at Juno
+            College of Technology (formerly HackerYou), where I completed the Web
+            Development Immersive bootcamp. This gave me all the tools necessary to become
+            a Front End developer.
           </p>
         </div>
 
@@ -135,15 +127,14 @@ const About = () => {
         </h2>
         <div>
           <p>
-            I have 2.5 years of professional experience as a Front End
-            Developer. My main focus has been component driven development.
-            Primarily working in Next.js, React, Tailwind and ScSS. In my short
-            time on this adventure, I&apos;ve already contributed to over a
-            dozen large scale projects. I have taken on freelance opportunities
-            where I have not only been the lead developer on these projects
-            (teams of up to 3), but also managed all communications between
-            myself and the clients. This includes drafting all the contracts and
-            have been the main point of contact for any of their needs.
+            I have 2.5 years of professional experience as a Front End Developer. My main
+            focus has been component driven development. Primarily working in Next.js,
+            React, Tailwind and ScSS. In my short time on this adventure, I&apos;ve
+            already contributed to over a dozen large scale projects. I have taken on
+            freelance opportunities where I have not only been the lead developer on these
+            projects (teams of up to 3), but also managed all communications between
+            myself and the clients. This includes drafting all the contracts and have been
+            the main point of contact for any of their needs.
           </p>
         </div>
 
@@ -153,18 +144,16 @@ const About = () => {
         </h2>
         <a
           className="mt-4 mr-4 btn-primary primary-grad"
-          href="https://firebasestorage.googleapis.com/v0/b/portfoliov3-6fb9a.appspot.com/o/project-photos%2FKeil%20Martin%20-%20Resume.pdf?alt=media&token=7c39d70d-69db-4fdd-b169-6c064ca5c1d0"
+          href="https://firebasestorage.googleapis.com/v0/b/portfoliov3-6fb9a.appspot.com/o/project-photos%2FKeil%20Martin%20-%20Resume.pdf?alt=media&token=980d9d3b-e8a2-4f13-a6c9-7287bab07610"
           target="_blank"
-          rel="noreferrer"
-        >
+          rel="noreferrer">
           View Resume
         </a>
         <a
           className="mt-4 btn-primary primary-grad"
           href="https://github.com/keilmart/portfolioV3"
           target="_blank"
-          rel="noreferrer"
-        >
+          rel="noreferrer">
           View Github
         </a>
       </div>
