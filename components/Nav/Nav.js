080a29854639450b50e852fbc614617a3b3c8aaa
keil <keilwmartin@gmail.com>
2025-01-11 10:39:55 -0500

    Revert back to original

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -7,25 +7,7 @@ import ThemeToggle from "./ThemeToggle";
 import { CircleIcon } from "./NavIcons/Index";
 
 const Nav = () => {
-  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
   const router = useRouter();
-  const [isClient, setIsClient] = useState(false);
-  const [loading, setLoading] = useState(true); // Add loading state
-
-  useEffect(() => {
-    setIsClient(true); // Set client flag to true once the component is mounted in the client
-  }, []);
-
-  useEffect(() => {
-    // Ensure the component doesn't try to redirect while the authentication is in progress
-    if (isAuthenticated && loading) {
-      setLoading(false); // Set loading to false once authenticated
-      router.push("/budget"); // Manually redirect to /budget if authenticated
-    }
-  }, [isAuthenticated, loading, router]);
-
-  const redirectUrl = isClient ? `${window.location.origin}/budget` : "";
-  console.log("Redirect URL:", redirectUrl);
 
   const NavItem = ({ item }) => {
     return (
@@ -52,69 +34,30 @@ const Nav = () => {
       className={`sticky top-0 z-30 bg-white dark:bg-darkModeDetail ${
         router.pathname === "/budget" ? "" : "border-b border-primary dark:border-b-0"
       }`}>
-      <div className="relative">
-        <div
-          className={`flex items-center justify-between w-full px-2.5 py-3 mx-auto sm:px-4 transition-all duration-500 ease-in-out ${
-            router.pathname === "/budget"
-              ? "xl:px-12"
-              : "container max-w-screen-xl lg:px-8 "
-          }`}>
-          {router.pathname === "/budget" && (
-            <div className="absolute left-0 h-full w-96 bg-syncWave dark:bg-darkModeDetail"></div>
-          )}
-          {/* Logo Section */}
-          <div className="z-10 flex flex-row items-center justify-center text-sm antialiased font-semibold leading-3 tracking-wide text-primary dark:text-slate-200">
-            <Link href="/">
-              <a className="flex">
-                {router.pathname != "/budget" && <CircleIcon />}
-                <div className="flex items-center justify-center pl-navX pt-navY">
-                  <span
-                    className={`text-base antialiased tracking-tight sm:text-lg font-montserrat tracking dark:tracking-normal ${
-                      router.pathname === "/budget" ? "" : ""
-                    }`}>
-                    {router.pathname === "/budget" ? <>TripleTrack</> : <>TripleByte</>}
-                  </span>
-                </div>
-              </a>
-            </Link>
+      <div
+        className={`flex items-center justify-between w-full px-2.5 py-3 mx-auto sm:px-4 transition-all duration-500 ease-in-out container max-w-screen-xl lg:px-8 `}>
+        <div className="z-10 flex flex-row items-center justify-center text-sm antialiased font-semibold leading-3 tracking-wide text-primary dark:text-slate-200">
+          <Link href="/">
+            <a className="flex">
+              <CircleIcon />
+              <div className="flex items-center justify-center pl-navX pt-navY">
+                <span
+                  className={`text-base antialiased tracking-tight sm:text-lg font-montserrat tracking dark:tracking-normal `}>
+                  TripleByte
+                </span>
+              </div>
+            </a>
+          </Link>
+        </div>
+
+        <div className="flex items-center">
+          <div className="space-x-2.5 sm:space-x-5">
+            {navigation.map((item) => (
+              <NavItem item={item} key={item.name} />
+            ))}
           </div>
-
-          <div className="flex items-center">
-            <div className="space-x-2.5 sm:space-x-5">
-              {navigation.map((item) => (
-                <NavItem item={item} key={item.name} />
-              ))}
-            </div>
-            <div className="flex ml-3 sm:ml-5">
-              <ThemeToggle />
-            </div>
-
-            <div className="ml-4">
-              {!isAuthenticated && isClient && (
-                <button
-                  onClick={() => {
-                    const redirectUrl = "http://localhost:3000/budget"; // Hardcoded for testing
-                    console.log("Redirect URL:", redirectUrl); // Log the redirect URL
-                    loginWithRedirect({ redirect_uri: redirectUrl }); // Call the login redirect
-                  }}
-                  className="btn-primary primary-grad">
-                  Sign In
-                </button>
-              )}
-              {isAuthenticated && (
-                <button
-                  onClick={() =>
-                    logout({
-                      logoutParams: {
-                        returnTo: window.location.origin, // Redirect after logout
-                      },
-                    })
-                  }
-                  className="btn-primary primary-grad">
-                  Sign Out
-                </button>
-              )}
-            </div>
+          <div className="flex ml-3 sm:ml-5">
+            <ThemeToggle />
           </div>
         </div>
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,17 @@
 import Nav from "../components/Nav/Nav";
 import Footer from "../components/Footer/Footer";
 
-import { Auth0Provider } from "@auth0/auth0-react";
 import { ThemeProvider } from "next-themes";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  const isBrowser = typeof window !== "undefined"; // Check if the code is running on the client
-
   return (
-    <>
-      {isBrowser ? (
-        <Auth0Provider
-          // domain={process.env.AUTH0_PUBLIC_DOMAIN}
-          // clientId={process.env.AUTH0_PUBLIC_CLIENT_ID}
-          domain="keilmart.us.auth0.com"
-          clientId="GUC8ADXxT3GAZ633l1eMafCKLgx4ZBYs"
-          authorizationParams={{
-            redirect_uri: window.location.origin, // Ensures proper redirect URI
-          }}>
-          <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
-            <Nav />
-            <Component {...pageProps} />
-            <Footer />
-          </ThemeProvider>
-        </Auth0Provider>
-      ) : (
-        // Render without Auth0Provider during SSR
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
-          <Nav />
-          <Component {...pageProps} />
-
-          <Footer />
-        </ThemeProvider>
-      )}
-    </>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+      <Nav />
+      <Component {...pageProps} />
+      <Footer />
+    </ThemeProvider>
   );
 }
 
