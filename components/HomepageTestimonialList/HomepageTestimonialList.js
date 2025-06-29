786dce9c056b12e1cfb655a7bd11d2bada6a8deb
keil <keilwmartin@gmail.com>
2025-06-28 22:33:20 -0400

    Update roles

--- components/HomepageTestimonialList/HomepageTestimonialList.js
+++ components/HomepageTestimonialList/HomepageTestimonialList.js
@@ -7,7 +7,7 @@ const HomepageTestimonialList = () => {
       name: "Joel Bott",
       position: "30 Forensic Engineering",
       photo: "/pictures/joel-bott.png",
-      title: "30 Forensic",
+      title: "Team Lead",
       company: "TOK Design Studio",
       testimonial:
         "Keil and I worked together for almost a year, and throughout that time he proved an immensely valuable asset to both our team and our firm as a whole. During our time as colleagues, he ensured all work completed was done so in a timely manner and of the highest quality. He possesses a self-motivated attitude and overall personability which, when paired with his constant strive to further himself through continuing education, created someone who led by example and demonstrated that the road to success was paved with hard-work and a dedication to your craft. He is passionate about what he does and is continuously driven to create the finest product possible. I cannot hold Keil in higher regards.",
@@ -16,7 +16,7 @@ const HomepageTestimonialList = () => {
       name: "Amy Mejia",
       position: "York Region",
       photo: "/pictures/amy-mejia.png",
-      title: "York Region",
+      title: "C.E.T",
       company: "Tatham Engineering",
       testimonial:
         "Keil is very organized and consistent with how he keeps electronic files and hard copy files stored.He has great mathematical abilities and is able to communicate with anyone, which allows him to be very efficient and have a very strong work ethic. Keil is a very quick learner and uses it to his advantage when faced with any new challenges as well as helping others around him on programs such as AutoCAD. He is very reliable as he is able to meet all of his deadlines therefore, I believe that Keil would be a valuable asset and any company would greatly benefit from having him as part of their team.",
