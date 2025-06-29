549c990c4236d4b5c6f811ac6db9552d0666d76e
keil <keilwmartin@gmail.com>
2025-06-28 21:08:35 -0400

    Small tweak to skills title

--- components/HomepageListedSkills/HomepageListedSkills.js
+++ components/HomepageListedSkills/HomepageListedSkills.js
@@ -13,7 +13,7 @@ const HomepageListedSkills = () => {
   return (
     <div className="max-w-3xl px-5 py-6 mx-auto mb-10 text-center transition duration-500 ease-in-out bg-gray-100 border-2 border-gray-200 border-dotted rounded-lg mt-9 md:mb-20 md:mt-20 md:p-10 dark:bg-darkModeDetail dark:border-none">
       <h2 className="text-xl font-semibold md:text-2xl text-primary dark:text-slate-200">
-        (A few of my go-to technologies)
+        A few of my go-to technologies
       </h2>
       <div className="flex flex-wrap justify-center mt-6 sm:mx-9">
         <Capsule
